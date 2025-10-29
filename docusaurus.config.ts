6bdea6c935ac13c915b3ca3a5440e789c1cf4a24
Darren Fürst <darren.fuerst@pm.me>
2025-10-29 17:42:12 +0100

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,7 +72,7 @@ const config: Config = {
       title: 'ALARMiator',
       logo: {
         alt: 'ALARMiator Logo',
-        src: 'img/logo.svg',
+        src: 'img/alarmiator-logo.webp',
       },
       items: [
 
